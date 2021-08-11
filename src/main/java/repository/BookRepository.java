639bc44e9f677e4d523778808aa26842c23646a8
tuanvh <tuanvh@mpos.vn>
2021-08-11 14:19:57 +0700

    fix repo

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -2,8 +2,8 @@ package repository;
 
 import nextpay.vn.bookshop.model.Book;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
+
 
 public interface BookRepository extends CrudRepository<Book,Long> {
     List<Book> findByCategory(String category);
--- src/main/java/repository/BookToCartItemRepository.java
+++ src/main/java/repository/BookToCartItemRepository.java
@@ -5,11 +5,11 @@ import nextpay.vn.bookshop.model.CartItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Transactional
-    public interface BookToCartItemRepository extends CrudRepository<BookToCartItem, Long> {
+public interface BookToCartItemRepository extends CrudRepository<BookToCartItem, Long> {
 
         void deleteByCartItem(CartItem cartItem);
-
-    }
+}
 
 
