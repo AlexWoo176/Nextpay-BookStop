b18eb8686f30af65bffbda9fb2fe34429d62e5c8
tuanvh <tuanvh@mpos.vn>
2021-08-13 14:57:40 +0700

    admin-portal

---
+++ adminPortal/src/main/java/nextpay/vn/admin/repositoty/BookRepository.java
@@ -0,0 +1,8 @@
+package nextpay.vn.admin.repositoty;
+
+import nextpay.vn.admin.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
--- src/main/java/nextpay/vn/bookshop/repository/BookRepository.java
+++ src/main/java/nextpay/vn/bookshop/repository/BookRepository.java
@@ -4,7 +4,6 @@ import nextpay.vn.bookshop.model.Book;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
-
 public interface BookRepository extends CrudRepository<Book,Long> {
     List<Book> findByCategory(String category);
 
